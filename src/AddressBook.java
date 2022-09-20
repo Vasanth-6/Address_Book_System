3ef8213b068cfa623c2a46f502ddfc586cf185a4
vasu <vasanthakumarnr6@gmail.com>
2022-09-19 17:59:03 -0700

    added - Creating contact details

--- src/AddressBook.java
+++ src/AddressBook.java
@@ -1,9 +1,11 @@
+import java.util.Scanner;
+
 public class AddressBook {
 
     public static void main(String[] args) {
 
         System.out.println("Welcome to Address Book Program");
-        contact contact = new contact();
+       contact contact = new contact();
         contact.setContactDetails();
-    }
+         }
 }
